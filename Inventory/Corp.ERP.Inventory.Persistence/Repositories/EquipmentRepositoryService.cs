21e6b302c214161b3619532eb8c71931cb88e38c
touir1 <touir.mat@gmail.com>
2023-05-09 22:10:11 +0100

    crud repository implementation for Equipment entity

--- Inventory/Corp.ERP.Inventory.Persistence/Repositories/EquipmentRepositoryService.cs
+++ Inventory/Corp.ERP.Inventory.Persistence/Repositories/EquipmentRepositoryService.cs
@@ -20,15 +20,46 @@ public class EquipmentRepositoryService : IEquipmentRepositoryService
             .ToListAsync();
     }
 
-    public Task<IList<Equipment>> GetAllAsync(Predicate<Equipment> predicate) => throw new NotImplementedException();
+    public async Task<IList<Equipment>> GetAllAsync(Predicate<Equipment> predicate)
+    {
+        return await _inventoryContext.Set<Equipment>()
+            .Include(inc => inc.StorageUnit)
+            .Include(inc => inc.UsedBy)
+            .Where(w => predicate(w))
+            .ToListAsync();
+    }
 
-    public Task<Equipment> GetByIdAsync(Guid id) => throw new NotImplementedException();
+    public async Task<Equipment> GetByIdAsync(Guid id)
+    {
+        return await _inventoryContext.Set<Equipment>()
+            .Include(inc => inc.StorageUnit)
+            .Include(inc => inc.UsedBy)
+            .FirstAsync(w => w.Id == id);
+    }
 
-    public Task<Equipment> GetFirstOrDefaultAsync(Predicate<Equipment> predicate, Equipment defaultValue) => throw new NotImplementedException();
+    public async Task<Equipment> GetFirstOrDefaultAsync(Predicate<Equipment> predicate, Equipment defaultValue)
+    {
+        return await _inventoryContext.Set<Equipment>()
+            .Include(inc => inc.StorageUnit)
+            .Include(inc => inc.UsedBy)
+            .FirstAsync(w => predicate(w)) ?? defaultValue;
+    }
 
-    public Task UpdateAsync(Equipment entity) => throw new NotImplementedException();
+    public async Task UpdateAsync(Equipment entity)
+    {
+        _inventoryContext.Entry(entity).State = EntityState.Modified;
+        await _inventoryContext.SaveChangesAsync();
+    }
 
-    public Task AddAsync(Equipment entity) => throw new NotImplementedException();
+    public async Task AddAsync(Equipment entity)
+    {
+        await _inventoryContext.Set<Equipment>().AddAsync(entity);
+        await _inventoryContext.SaveChangesAsync();
+    }
 
-    public Task DeleteAsync(Equipment entity) => throw new NotImplementedException();
+    public async Task DeleteAsync(Equipment entity)
+    {
+        _inventoryContext.Set<Equipment>().Remove(entity);
+        await _inventoryContext.SaveChangesAsync();
+    }
 }
