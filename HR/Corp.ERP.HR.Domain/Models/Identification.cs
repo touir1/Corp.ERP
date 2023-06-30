5c16cfda429cba276b4e8d8d6c1d3d78abd8b444
touir1 <touir.mat@gmail.com>
2023-06-30 18:28:19 +0100

    Adding IdentificationType to domain

--- HR/Corp.ERP.HR.Domain/Models/Identification.cs
+++ HR/Corp.ERP.HR.Domain/Models/Identification.cs
@@ -3,10 +3,10 @@
 public class Identification : IEntity
 {
     public Guid Id { get; set; }
-    public virtual string Code { get; set; }
-    public virtual string Label { get; set; }
-    public virtual string Description { get; set; }
+    public virtual IdentificationType IdentificationType { get; set; }
+    public virtual User Owner { get; set; }
     public virtual string IdNumber { get; set; }
+
     public virtual DateTime StartDate { get; set; }
     public virtual DateTime? ExpiryDate { get; set;}
 }
---
+++ HR/Corp.ERP.HR.Domain/Models/IdentificationType.cs
@@ -0,0 +1,9 @@
+﻿namespace Corp.ERP.HR.Domain.Models;
+
+public class IdentificationType : IEntity
+{
+    public Guid Id { get; set; }
+    public virtual string Code { get; set; }
+    public virtual String Label { get; set; }
+    public virtual String Description { get; set; }
+}
